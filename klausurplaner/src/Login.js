c5c251d3d8cf5999cdc45f6d281355256b5a3dfb
Marius <mariushannappel@gmail.com>
2023-09-11 10:10:54 +0200

    TestMarius

--- klausurplaner/src/Login.js
+++ klausurplaner/src/Login.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import { TextField, Container } from '@mui/material';
+import { TextField, Container, Button } from '@mui/material';
 import './Login.css';
 
 
 function Login(props) {
     return (
-        <div>
             <Container id='login-panel'>
-                <Grid container spacing={2}>
-
-                        <Grid xs={4}>
-                            <TextField id="input-login" label="Email-Adresse" variant='outlined'/>
-                            <button>Test</button> 
-                        </Grid>
-                        <Grid xs={4}>
-                            <button>Test</button> 
-                        </Grid>
+            <Grid container direction={'column'} spacing={2}>
+                <Grid>
+                    <TextField className="input-login" id="input-email" label="Email-Adresse" variant='outlined'/>
                 </Grid>
-            </Container>
+                <Grid>
+                    <TextField className="input-login" id="input-password" label="Email-Adresse" variant='outlined'/>
+                </Grid>
+                <Grid xs={1}>
+                    <Button variant='outlined'>Login</Button>
+                </Grid>
+            </Grid>
 
-        </div>
+
+            </Container>
     );
 }
 
