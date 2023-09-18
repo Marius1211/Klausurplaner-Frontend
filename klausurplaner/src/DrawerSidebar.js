f6343b8dd41ca104202a23cb9cac59259f475c9a
Paaad <patrick.wagner2@t-online.de>
2023-09-18 12:37:22 +0200

    Imports entfernt

--- klausurplaner/src/DrawerSidebar.js
+++ klausurplaner/src/DrawerSidebar.js
@@ -16,8 +16,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import AlarmIcon from '@mui/icons-material/Alarm';
 import AddBoxIcon from '@mui/icons-material/AddBox';
