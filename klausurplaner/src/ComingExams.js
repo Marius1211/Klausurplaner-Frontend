fdedac55f22f8dd81489fdd1ba22f48558264c0a
Paaad <patrick.wagner2@t-online.de>
2023-09-19 12:03:12 +0200

    Anstehende Klusuren begonnen Header schon eingefügt

---
+++ klausurplaner/src/ComingExams.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './ComingExams.css'
+import DrawerSidebar from './DrawerSidebar'
+
+function ComingExams(){
+    return(
+        <DrawerSidebar header="Anstehende Klausuren"/>
+    );
+}
+
+export default ComingExams;
--- klausurplaner/src/index.js
+++ klausurplaner/src/index.js
@@ -6,6 +6,7 @@ import Login from './Login';
 import AddKlausur from './AddKlausur';
 import ClassOverview from './ClassOverview';
 import Calendar from './Calendar'
+import ComingExams from './ComingExams';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -16,7 +17,7 @@ import '@mui/icons-material'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calendar/>
+    <ComingExams/>
   </React.StrictMode>
 );
 
